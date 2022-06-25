516a4a1087f58f3a952a47886af7649c64074ca6
yashlan <yashlan007@gmail.com>
2022-06-25 14:00:56 +0700

    initial commit

---
+++ app/src/main/java/com/yashlan/androidexpertsub1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yashlan.androidexpertsub1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yashlan.androidexpertsub1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+}
