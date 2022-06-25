edf0d74e56d282054108da466c7983c65b47710c
yashlan <yashlan007@gmail.com>
2022-06-26 02:32:23 +0700

    Create MyApplication.kt

---
+++ app/src/main/java/com/yashlan/androidexpertsub1/MyApplication.kt
@@ -0,0 +1,36 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/26/22, 12:41 AM
+ * Last modified 6/26/22, 12:41 AM
+ */
+
+package com.yashlan.androidexpertsub1
+
+import android.app.Application
+import com.yashlan.androidexpertsub1.di.useCaseModule
+import com.yashlan.androidexpertsub1.di.viewModelModule
+import com.yashlan.core.di.databaseModule
+import com.yashlan.core.di.networkModule
+import com.yashlan.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.logger.Level
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
+}
