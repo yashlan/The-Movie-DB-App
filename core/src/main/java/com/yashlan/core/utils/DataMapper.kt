4a5683d74083819064e0e0913d814d474d3fc07f
yashlan <yashlan007@gmail.com>
2022-06-26 02:32:41 +0700

    Create DataMapper.kt

---
+++ core/src/main/java/com/yashlan/core/utils/DataMapper.kt
@@ -0,0 +1,49 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 10:24 PM
+ * Last modified 6/25/22, 10:24 PM
+ */
+
+package com.yashlan.core.utils
+
+import com.yashlan.core.data.source.local.entity.MovieEntity
+import com.yashlan.core.data.source.remote.response.MovieResponse
+import com.yashlan.core.domain.model.Movie
+
+object DataMapper {
+    fun mapResponsesToEntities(input: List<MovieResponse.Results>): List<MovieEntity> {
+        val movieList = ArrayList<MovieEntity>()
+        input.map {
+            val tourism = MovieEntity(
+                movieId = it.movieId,
+                title = it.title,
+                releaseDate = it.releaseDate,
+                overview = it.overview,
+                posterPath = it.posterPath,
+                isFavorite = false
+            )
+            movieList.add(tourism)
+        }
+        return movieList
+    }
+
+    fun mapEntitiesToDomain(input: List<MovieEntity>): List<Movie> =
+        input.map {
+            Movie(
+                movieId = it.movieId,
+                title = it.title,
+                releaseDate = it.releaseDate,
+                overview = it.overview,
+                posterPath = it.posterPath,
+                isFavorite = it.isFavorite
+            )
+        }
+
+    fun mapDomainToEntity(input: Movie) = MovieEntity(
+        movieId = input.movieId,
+        title = input.title,
+        releaseDate = input.releaseDate,
+        overview = input.overview,
+        posterPath = input.posterPath,
+        isFavorite = input.isFavorite
+    )
+}
