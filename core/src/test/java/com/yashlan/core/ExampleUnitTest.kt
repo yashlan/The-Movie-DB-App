554cbbca9f07fdf403c272380370c5d27e86c3b2
yashlan <yashlan007@gmail.com>
2022-06-25 17:11:42 +0700

    Create ExampleUnitTest.kt

---
+++ core/src/test/java/com/yashlan/core/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 4:19 PM
+ * Last modified 6/25/22, 2:08 PM
+ */
+
+package com.yashlan.core
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
