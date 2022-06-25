6befc4ac8e6f05b0a0c6378e7c2891f12e2b2cb4
yashlan <yashlan007@gmail.com>
2022-06-26 02:35:23 +0700

    Create HomeViewModel.kt

---
+++ app/src/main/java/com/yashlan/androidexpertsub1/home/HomeViewModel.kt
@@ -0,0 +1,14 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 4:19 PM
+ * Last modified 6/25/22, 4:03 PM
+ */
+
+package com.yashlan.androidexpertsub1.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.yashlan.core.domain.usecase.MovieUseCase
+
+class HomeViewModel(movieUseCase: MovieUseCase) : ViewModel() {
+    val movie = movieUseCase.getAllMovie().asLiveData()
+}
