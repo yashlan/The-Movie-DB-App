0138fd9608db918b5a9b5ab2914b06f28e78bc1e
yashlan <yashlan007@gmail.com>
2022-06-26 02:33:42 +0700

    Create ApiService.kt

---
+++ core/src/main/java/com/yashlan/core/data/source/remote/network/ApiService.kt
@@ -0,0 +1,16 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 10:26 PM
+ * Last modified 6/25/22, 10:26 PM
+ */
+
+package com.yashlan.core.data.source.remote.network
+
+import com.yashlan.core.BuildConfig
+import com.yashlan.core.data.source.remote.response.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface ApiService {
+    @GET("discover/movie?api_key=${BuildConfig.API_KEY_V3}")
+    suspend fun getListMovie(): MovieResponse
+}
