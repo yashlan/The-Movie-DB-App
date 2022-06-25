2e6fde095b78be7308289d8172b1b631d2412fcd
yashlan <yashlan007@gmail.com>
2022-06-26 02:33:53 +0700

    Create LocalDataSource.kt

---
+++ core/src/main/java/com/yashlan/core/data/source/local/LocalDataSource.kt
@@ -0,0 +1,24 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 9:57 PM
+ * Last modified 6/25/22, 9:57 PM
+ */
+
+package com.yashlan.core.data.source.local
+
+import com.yashlan.core.data.source.local.entity.MovieEntity
+import com.yashlan.core.data.source.local.room.MovieDao
+import kotlinx.coroutines.flow.Flow
+
+class LocalDataSource(private val movieDao: MovieDao) {
+
+    fun getAllMovie(): Flow<List<MovieEntity>> = movieDao.getAllMovie()
+
+    fun getFavoriteMovie(): Flow<List<MovieEntity>> = movieDao.getFavoriteMovie()
+
+    suspend fun insertMovie(movieList: List<MovieEntity>) = movieDao.insertMovie(movieList)
+
+    fun setFavoriteMovie(movie: MovieEntity, newState: Boolean) {
+        movie.isFavorite = newState
+        movieDao.updateFavoriteMovie(movie)
+    }
+}
