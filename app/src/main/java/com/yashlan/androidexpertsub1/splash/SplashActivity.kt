e5d75b33e2f988ef730221b1f3d6ba3f2c108760
yashlan <yashlan007@gmail.com>
2022-06-30 20:20:51 +0700

    Create SplashActivity.kt

---
+++ app/src/main/java/com/yashlan/androidexpertsub1/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+/*
+ * Created by Muhammad Yashlan Iskandar on 6/25/22, 4:21 PM
+ * Last modified 6/25/22, 4:21 PM
+ */
+
+package com.yashlan.androidexpertsub1.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.yashlan.androidexpertsub1.MainBottomNavActivity
+import com.yashlan.androidexpertsub1.R
+import com.yashlan.core.utils.forcePortraitScreenOrientation
+import com.yashlan.core.utils.setFullscreen
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        forcePortraitScreenOrientation()
+        setFullscreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainBottomNavActivity::class.java))
+            finish()
+        }, DELAY)
+    }
+
+    companion object {
+        private const val DELAY: Long = 1000
+    }
+}
