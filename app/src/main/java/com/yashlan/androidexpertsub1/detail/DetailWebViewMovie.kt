7bf79ca237618b56b934ded683f2d1e300d241fd
yashlan <yashlan007@gmail.com>
2022-07-12 20:59:31 +0700

    Update DetailWebViewMovie.kt

--- app/src/main/java/com/yashlan/androidexpertsub1/detail/DetailWebViewMovie.kt
+++ app/src/main/java/com/yashlan/androidexpertsub1/detail/DetailWebViewMovie.kt
@@ -3,39 +3,43 @@
  * Last modified 6/30/22, 9:30 PM
  */
 
-/*
- * Created by Muhammad Yashlan Iskandar on 7/1/22, 7:56 PM
- * Last modified 6/30/22, 9:30 PM
- */
-
-/*
- * Created by Muhammad Yashlan Iskandar on 6/26/22, 9:34 PM
- * Last modified 6/26/22, 9:34 PM
- */
-
 package com.yashlan.androidexpertsub1.detail
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import com.yashlan.androidexpertsub1.databinding.ActivityDetailWebViewMovieBinding
 import com.yashlan.core.BuildConfig
-import com.yashlan.core.utils.forcePortraitScreenOrientation
-import com.yashlan.core.utils.setFullscreen
+import com.yashlan.core.utils.ScreenUtil
 
+@SuppressLint("SetJavaScriptEnabled")
 class DetailWebViewMovie : AppCompatActivity() {
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private lateinit var binding: ActivityDetailWebViewMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        forcePortraitScreenOrientation()
-        setFullscreen()
+        ScreenUtil.forcePortraitScreenOrientation(this)
+        ScreenUtil.setFullscreen(this)
         super.onCreate(savedInstanceState)
-        
-        val myWebView = WebView(applicationContext)
-        myWebView.settings.javaScriptEnabled = true
-        setContentView(myWebView)
+        binding = ActivityDetailWebViewMovieBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val id = intent.getIntExtra(MOVIE_ID_EXTRA, 0)
-        myWebView.loadUrl(BuildConfig.WEBVIEW_URL + id)
+
+        binding.webContainer.apply {
+            settings.javaScriptEnabled = true
+            loadUrl(BuildConfig.WEBVIEW_URL + id)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.webContainer.destroy()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.webContainer.destroy()
     }
 
     companion object {
