e75e65df2bf8325125483a75dd618c4a0907cac5
yashlan <yashlan007@gmail.com>
2022-07-12 22:15:59 +0700

    Update DetailWebViewMovie.kt

--- app/src/main/java/com/yashlan/androidexpertsub1/detail/DetailWebViewMovie.kt
+++ app/src/main/java/com/yashlan/androidexpertsub1/detail/DetailWebViewMovie.kt
@@ -8,38 +8,36 @@ package com.yashlan.androidexpertsub1.detail
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.yashlan.androidexpertsub1.databinding.ActivityDetailWebViewMovieBinding
 import com.yashlan.core.BuildConfig
+import com.yashlan.core.customview.CustomWebView
 import com.yashlan.core.utils.ScreenUtil
 
 @SuppressLint("SetJavaScriptEnabled")
 class DetailWebViewMovie : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDetailWebViewMovieBinding
+    private lateinit var webContainer: CustomWebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         ScreenUtil.forcePortraitScreenOrientation(this)
         ScreenUtil.setFullscreen(this)
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailWebViewMovieBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        webContainer = CustomWebView(this)
         val id = intent.getIntExtra(MOVIE_ID_EXTRA, 0)
-
-        binding.webContainer.apply {
+        webContainer.apply {
             settings.javaScriptEnabled = true
             loadUrl(BuildConfig.WEBVIEW_URL + id)
         }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        binding.webContainer.destroy()
+        setContentView(webContainer)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        binding.webContainer.destroy()
+        webContainer.apply {
+            removeAllViews()
+            removeAllViewsInLayout()
+            destroy()
+        }
     }
 
     companion object {
