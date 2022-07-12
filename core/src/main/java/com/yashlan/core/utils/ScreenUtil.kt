346db3c83db209a51b62ed6ee4711ae70d04f2cf
yashlan <yashlan007@gmail.com>
2022-07-12 20:56:17 +0700

    Update ScreenUtil.kt

--- core/src/main/java/com/yashlan/core/utils/ScreenUtil.kt
+++ core/src/main/java/com/yashlan/core/utils/ScreenUtil.kt
@@ -12,29 +12,31 @@ import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
-fun AppCompatActivity.setFullscreen() {
-    when {
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
-            window.attributes.layoutInDisplayCutoutMode =
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-        }
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.apply {
-                hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+object ScreenUtil {
+    fun setFullscreen(app: AppCompatActivity) {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
+                app.window.attributes.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                app.window.setDecorFitsSystemWindows(false)
+                app.window.insetsController?.apply {
+                    hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                    systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            }
+            else -> {
+                app.window.setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                )
             }
         }
-        else -> {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-            )
-        }
+        app.supportActionBar?.hide()
     }
-    supportActionBar?.hide()
-}
 
-fun AppCompatActivity.forcePortraitScreenOrientation() {
-    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR
+    fun forcePortraitScreenOrientation(app: AppCompatActivity) {
+        app.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_NOSENSOR
+    }
 }
